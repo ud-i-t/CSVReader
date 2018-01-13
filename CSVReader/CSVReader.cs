499311e7af528af12ffefcae0cfe6e463ec031e2
mizunoawa.web <saeki_takatoshi@outlook.jp>
2018-01-13 19:06:38 +0900

    first commit

---
+++ CSVReader/CSVReader.cs
@@ -0,0 +1,92 @@
+﻿using Microsoft.VisualBasic.FileIO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RLF3
+{
+    class CSVReader
+    {
+        public static List<UserData> Parse(string FileName)
+        {
+            TextFieldParser parser = new TextFieldParser(FileName, Encoding.GetEncoding("Shift_JIS"));
+            parser.TextFieldType = FieldType.Delimited;
+            parser.SetDelimiters(",");
+
+            string[] keys = parser.ReadFields();
+            foreach(var k in keys.Select((v, i) => new { v, i }))
+            {
+                keys[k.i] = k.v.ToLower();
+            }
+
+            var tbl = new List<UserData>();
+
+            while (parser.EndOfData == false)
+            {
+                string[] column = parser.ReadFields();
+                var row = new UserData();
+
+                foreach (var c in column.Select((v, i) => new { v, i }))
+                {
+                    row.Add(keys[c.i], c.v);
+                }
+                tbl.Add(row);
+            }
+
+            return tbl;
+        }
+
+        public static List<T> Parse<T>(string FileName) where T : new()
+        {
+            TextFieldParser parser = new TextFieldParser(FileName, Encoding.GetEncoding("Shift_JIS"));
+            parser.TextFieldType = FieldType.Delimited;
+            parser.SetDelimiters(",");
+
+            string[] keys = parser.ReadFields();
+            foreach (var k in keys.Select((v, i) => new { v, i }))
+            {
+                keys[k.i] = k.v.ToLower();
+            }
+
+            var tbl = new List<T>();
+
+            while (parser.EndOfData == false)
+            {
+                string[] column = parser.ReadFields();
+                var row = new T();
+                Type t = row.GetType();
+
+                foreach (var c in column.Select((v, i) => new { v, i }))
+                {
+                    var p = t.GetProperty(keys[c.i]);
+
+                    // プロパティが存在しない場合
+                    if(p == null)
+                    {
+                        continue;
+                    }
+
+                    var val = c.v;
+                    if (p.PropertyType.Equals(typeof(Int32))) {
+                        int v = val == "" ? -1 : Int32.Parse(val);
+                        p.SetValue(row, Convert.ToInt32(v));
+                    }
+                    else
+                    {
+                        p.SetValue(row, val);
+                    }
+                    
+                }
+                //データセット後処理
+                var afterDataset = t.GetMethod("AfterDataSet");
+                if (afterDataset != null) afterDataset.Invoke(row, null);
+
+                tbl.Add(row);
+            }
+
+            return tbl;
+        }
+    }
+}
---
+++ CSVReader/UserData.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RLF3
+{
+    public class UserData
+    {
+        private Dictionary<string, string> dataDic;
+
+        public UserData()
+        {
+            dataDic = new Dictionary<string, string>();
+        }
+
+        public string this[string key] {
+            get { return dataDic[key]; }
+            private set { dataDic[key] = value; }
+        }
+
+        public void Add(string key, string data)
+        {
+            dataDic.Add(key, data);
+        }
+
+        public bool ContainsKey(string key)
+        {
+            return dataDic.ContainsKey(key);
+        }
+    }
+}
